b18c5cce79cf51cac1af0b24c6276a039e108d99
qfoxb <42038239+qfoxb@users.noreply.github.com>
2022-01-17 18:47:48 -0600

    why did i write an outdated format smh

--- LoadLibraryInstaller.lua
+++ LoadLibraryInstaller.lua
@@ -1,9 +1,9 @@
 --GearDB Auto Setup Script for LoadLibrary
--- "LoadLibrary is a legacy mechanism that we previously used to provide access to Roblox-maintained Lua code. However, we haven’t updated the list of libraries or the libraries themselves in ages. " - Zeuxcg
+-- "LoadLibrary is a legacy mechanism that we previously used to provide access to Roblox-maintained Lua code. However, we havenâ€™t updated the list of libraries or the libraries themselves in ages. " - Zeuxcg
 -- It installs a copy of the rbxmx file provided on the devforum post "LoadLibrary is going to be removed on February 3rd"
 local InsertService = game:GetService("InsertService")
 local LoadLibrary = InsertService:LoadAsset(8533315553)
 LoadLibrary.Parent = game.ReplicatedStorage
 print("GearDB Auto Install complete")
 
--- see, it's simple. you could've just installed the rbxmx lol
+-- see, it's simple. you could've just installed the rbxm lol
