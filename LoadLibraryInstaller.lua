1aa6b82a9124bd1ca509f02d3299ccbf2c80fc15
qfoxb <42038239+qfoxb@users.noreply.github.com>
2022-10-17 18:15:48 -0500

    Update gitignore and loadlibraryinstaller

--- LoadLibraryInstaller.lua
+++ LoadLibraryInstaller.lua
@@ -3,7 +3,7 @@
 -- It installs a copy of the rbxmx file provided on the devforum post "LoadLibrary is going to be removed on February 3rd"
 local InsertService = game:GetService("InsertService")
 local LoadLibrary = InsertService:LoadAsset(8533315553)
-LoadLibrary.Parent = game.ReplicatedStorage
+LoadLibrary.LoadLibrary.Parent = game.ReplicatedStorage
 print("GearDB Auto Install complete")
 
 -- see, it's simple. you could've just installed the modelfile from the github description
